6740a1924d9ea4d617dc78a316c4a170d4a16a07
千残月 <438955580@qq.com>
2018-11-13 18:50:25 +0800

    2018-11-13

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
-Vue.use(VueAwesomeSwiper)
+Vue.use(VueAwesomeSwiper) 
 
 /* eslint-disable no-new */
 new Vue({
