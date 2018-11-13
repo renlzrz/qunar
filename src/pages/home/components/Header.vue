d7e084f0438760af5835261a4f018b47f8a4e8dd
千残月 <438955580@qq.com>
2018-11-13 16:49:40 +0800

    2018-11-13

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -2,7 +2,7 @@
   <div class="header">
     <div class="header-left"><span class="iconfont">&#xe624;</span></div>
     <div class="header-input"><span class="iconfont">&#xe632;</span>输入城市景点</div>
-    <div class="header-right">城市<span class="iconfont">&#xe64a;</span></div>
+    <div class="header-right">城市<span class="arrow-icon iconfont ">&#xe64a;</span></div>
   </div>
 </template>
 
@@ -13,6 +13,8 @@
 </script>
 
 <style lang="less" scoped>
+
+
   .header {
     height: 0.86rem;
     line-height: 0.86rem;
@@ -23,9 +25,13 @@
       width: 0.64rem;
       float: left;
     }
-    .header-righr {
-      width: 1.24rem;
+    .header-right {
+      width: 1.1rem;
       float: right;
+      .arrow-icon {
+        font-size: 0.3rem;
+        margin-left: 0.1rem;
+      }
     }
     .header-input {
       flex: 1;
