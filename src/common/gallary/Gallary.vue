d53c1260de28e4c0bdcde65e477f1fb4b02d7658
千残月 <438955580@qq.com>
2018-11-19 11:35:29 +0800

    2018-11-19 11:30

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper>
+        <!-- slides -->
+        <swiper-slide :options="swiperOption" v-for="(item,index) in imgs"
+        :key="index">
+          <img :src="item" class="gallary-img">
+        </swiper-slide>
+
+        <div class="swiper-pagination"  slot="pagination" style=""></div>
+      </swiper>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "CommonGallary",
+    props: {
+      imgs: {
+        default () {
+          return ['../../../static/20181118111523.jpg']
+        }
+      }
+    },
+    data:function () {
+      return {
+        swiperOption : {
+          pagination: '.swiper-pagination',
+          paginationType: 'fraction',
+          observeParents: true,
+          observe: true
+        }
+      }
+    },
+    methods: {
+      handleGallaryClick () {
+        this.$emit('close')
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .container >>> .swiper-container {
+    overflow: inherit
+  }
+  .container {
+    display: flex
+    flex-direction: column
+    justify-content: center
+    z-index: 99
+    position: fixed
+    left: 0
+    right:0
+    top:0
+    bottom:-4rem
+    background: #000
+    .wrapper {
+      overflow: hidden
+      height: 0
+      width: 100%
+      padding-bottom: 100%
+      .gallary-img {
+        width:100%
+      }
+      .swiper-pagination {
+        color: #fff
+        bottom: -1rem
+      }
+    }
+  }
+
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="../../../../static/20181118111523.jpg" alt="">
+      <div class="banner-info">
+        <div class="banner-tittle">大连圣亚海洋世界</div>
+        <div class="banner-number"><span class="iconfont banner-fcon">&#xe692;</span>39</div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="showGallary"
+    @close="handleGallaryClose">
+
+    </common-gallary>
+  </div>
+
+</template>
+
+<script>
+  import CommonGallary from 'common/gallary/Gallary'
+  export default {
+    name: "DetailBanner",
+    data: function () {
+      return {
+        showGallary:false,
+        imgs:['../../../static/20181118111523.jpg']
+      }
+
+    },
+    methods: {
+      handleBannerClick () {
+        this.showGallary = true
+      },
+      handleGallaryClose () {
+        this.showGallary = false
+      }
+    },
+    components: {
+      CommonGallary
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .banner {
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom:55%
+    .banner-img {
+      width:100%
+    }
+    .banner-info {
+      display: flex
+      position: absolute
+      left:0
+      right:0
+      bottom:0.44rem
+      line-height:0.6rem
+      color: #fff
+      background-image:linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,0.8))
+      .banner-tittle {
+        flex:1
+        font-style: 0.32rem
+        padding:0.2rem
+      }
+      .banner-number {
+        margin-top: 0.22rem
+        padding:0.2rem
+        line-height:0.12rem
+        height:0.12rem
+        border-radius 0.2rem
+        background: rgba(0,0,0,0.8)
+        font-style: 0.24rem
+        .banner-icon {
+          font-style: 0.24rem
+        }
+      }
+    }
+  }
+
+</style>
